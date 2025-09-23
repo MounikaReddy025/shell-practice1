28bd9f13ea633e71b7489eb8761cdc7f682d67ad
mounika <info@joindevops.com>
2025-09-23 06:49:33 +0530

    session-06

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,10 +4,12 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
     echo "ERROR:: Installation MYSQL is failure"
+    exit 1
 else
     echo "Installating MYSQL is SUCCESS"
 fi
