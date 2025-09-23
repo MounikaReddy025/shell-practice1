7ba4122bff32f751cc3099a9b8f4e5455af2ac1a
mounika <info@joindevops.com>
2025-09-23 07:34:01 +0530

    session-06

---
+++ 13-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installation $2 is failure"
+        exit 1
+    else
+        echo "Installating $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
