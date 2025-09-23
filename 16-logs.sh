a61702657857ecefd360b988241b89492564ed0e
mounika <info@joindevops.com>
2025-09-23 09:39:22 +0530

    session-06

---
+++ 16-logs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(data)"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ #functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installating $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installating $2 ... $G is SUCCESS $N"
+    fi
+}
+
+dnf list installed mysql &>>$LOG_FILE
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MYSQL"
+else
+    echo -e "MYSQL already exist ...$Y SKIPPING $N"
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ...$Y SKIPPING $N"
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e "python3 already exist ...$Y SKIPPING $N"
+fi
+
+
