63bf40cbc6384b06df3ca96c5c74d169746af3a8
mounika <info@joindevops.com>
2025-09-27 22:19:41 +0530

    session-06

--- 19-set.sh
+++ 19-set.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
 
+trap(){
+    echo "Ther is an error"
+}
+
+trap error ERR
 
 echo "Hello.."
 echo "Before error.."
-ccaffjl;dnf
+ccaffjl;dnf #here shell understands there is an error and signal is ERR
 echo "After error"
 
 
