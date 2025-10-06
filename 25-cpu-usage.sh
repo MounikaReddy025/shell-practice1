69b9d312f39319eb20baaf593966ca3811620d21
mounika <info@joindevops.com>
2025-10-06 13:43:11 +0530

    session-06

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # in project we keep it as 75
 
 while IFS= read -r line
 do
-    echo "Line: $line"
+    USAGE = echo $line | awk '{print $6}' |
 done <<< $DISK_USAGE
