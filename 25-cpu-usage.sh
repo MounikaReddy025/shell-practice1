e21168800b089e985e5f187a3a1d950b1c0ff857
mounika <info@joindevops.com>
2025-10-06 14:20:12 +0530

    session-06

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -10,7 +10,6 @@ do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        #echo "High Usage on $PARTITION: $USAGE"
         MESSAGE+="High Disk usage on $PARTITION: $USAGE %"
     fi
 done <<< $DISK_USAGE
