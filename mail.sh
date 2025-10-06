44d9706cff4011d553adb67224e6e0f78577de4e
mounika <info@joindevops.com>
2025-10-06 16:10:29 +0530

    session-06

--- mail.sh
+++ mail.sh
@@ -7,13 +7,13 @@ MESSAGE_BODY=$4
 IP_ADDRESS=$5
 TO_TEAM=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE_BODY/$MESSAGE_BODY/g" )
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE_BODY/$MESSAGE_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
-echo "FINAL_BODY"
+echo "$FINAL_BODY"
 } | msmtp "$TO_ADDRESS"
 
